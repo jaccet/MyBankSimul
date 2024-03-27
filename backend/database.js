406208af03fc17abcac36838ba99d2058d4afb7d
jaccet <jaakkelsoni@gmail.com>
2024-03-27 13:38:49 +0200

    muutettu toiminta .env-yhteensopivaksi ja lisätty authorization jsonwebtokenilla

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'bankAdmin',
-  password: 'putin1234',
-  database: 'bankSimul'
-});
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = mysql.createPool(process.env.SQL_SERVER);
 
 module.exports = connection;

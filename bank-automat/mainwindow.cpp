9abf00a044ae5e191f726ec7f10c39d02b666412
Tomi Ahola <tomiahola@hotmail.com>
2024-04-17 13:34:26 +0300

    turha stylesheet veks

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -13,7 +13,6 @@ MainWindow::MainWindow(QWidget *parent)
     rfidPtr = new rfid(this);
     rfidPtr->openPort();
     connect(ui->INSERT_CARD_BT,SIGNAL(clicked(bool)), this,SLOT(handleInserCardClick()));
-    this->setStyleSheet("background-color: lightblue;");
     connect(rfidPtr->serialPort, SIGNAL(readyRead()), this,SLOT(handleInserCardClick()));
     restPtr = new REST_API;
     connect(restPtr, SIGNAL(cardChecked(bool)), this, SLOT(receiveCardCheck(bool)));

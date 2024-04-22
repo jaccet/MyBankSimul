5e061ea16475698e373f3d883667cbbc5beb82aa
Samhyo <samuli.hyotyla@gmail.com>
2024-04-22 14:18:05 +0300

    bengalin ui muutos tiikeri

--- bank-automat/main.cpp
+++ bank-automat/main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file("C:\SPL_Group4\bank-automat\styles");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    a.setStyleSheet(styleSheet);
+
     MainWindow w;
     w.show();
     return a.exec();
